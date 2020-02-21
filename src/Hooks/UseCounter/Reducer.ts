92f82c9566edd17271a6eeb1027adfc18a1d51f4
Alexander Boutin Flegel <alexander.boutinFlegel@propellerhead.co.nz>
2020-02-21 14:41:11 +1300

    Created a Proof of Concept hook with listeners     Connects to a SignalR server     Added a generic hook for signal r endpoints

---
+++ src/Hooks/UseCounter/Reducer.ts
@@ -0,0 +1,17 @@
+import { SignalState } from "./State";
+import { Increment, IncrementAction } from "./Actions/Increment";
+import { Decrement, DecrementAction } from "./Actions/Decrement";
+
+export type SignalStateActions = Increment | Decrement;
+
+export const Reducer = (state: SignalState, action: SignalStateActions): SignalState => {
+
+	switch (action.type) {
+		case IncrementAction:
+			return { ...state, isLoading: false, counter: state.counter + action.value, change: action.value };
+		case DecrementAction:
+			return { ...state, isLoading: false, counter: state.counter - action.value, change: action.value };
+		default:
+			return state;
+	}
+};
