841dfe0146cd26fefa006fff06b3b0798da874a9
Alexander Boutin Flegel <alex.g.flegel@gmail.com>
2021-03-23 14:49:27 +1300

    [migration] updated packages (#6)
    migrated to new signalr package
    migrated to eslint
    eslint fixes
    
    Co-authored-by: Alexander Boutin Flegel <alexander.boutinflegel@propellerhead.co.nz>

--- src/Hooks/SignalR/SignalState.ts
+++ src/Hooks/SignalR/SignalState.ts
@@ -1,45 +1,47 @@
-import { useRef, useEffect, useCallback, useState } from "react";
-import { HubConnectionBuilder } from "@aspnet/signalr";
+import { useRef, useState } from "react";
+import { HubConnectionBuilder } from "@microsoft/signalr";
 import { useSignalREndpoint } from "./SignalREndpoint";
 
 export interface SignalState {
-  counter: number;
-  group: string;
+	counter: number;
+	group: string;
 }
 
 export const InitialState: SignalState = {
-  group: "",
-  counter: 0,
+	group: "",
+	counter: 0,
 };
 
 export const useSignalState = (baseUrl: string, companyId: string) => {
-  const [counter, setCounter] = useState<SignalState>({
-    counter: 0,
-    group: companyId,
-  });
+	const [counter, setCounter] = useState<SignalState>({
+		counter: 0,
+		group: companyId,
+	});
 
-  const connection = useRef(
-    new HubConnectionBuilder().withUrl(baseUrl).build()
-  );
+	const connection = useRef(
+		new HubConnectionBuilder().withUrl(baseUrl).build()
+	);
 
-  const incrementAsync = useSignalREndpoint<number, number>(
-    connection,
-    "Increment",
-    companyId,
-    (data) => setCounter({
-      ...counter,
-      counter: counter.counter + data,
-    })
-  );
-  const decrementAsync= useSignalREndpoint<number, number>(
-    connection,
-    "Decrement",
-    companyId,
-    (data) => setCounter({
-      ...counter,
-      counter: counter.counter - data,
-    })
-  );
+	const incrementAsync = useSignalREndpoint<number, number>(
+		connection,
+		"Increment",
+		companyId,
+		(data) =>
+			setCounter({
+				...counter,
+				counter: counter.counter + data,
+			})
+	);
+	const decrementAsync = useSignalREndpoint<number, number>(
+		connection,
+		"Decrement",
+		companyId,
+		(data) =>
+			setCounter({
+				...counter,
+				counter: counter.counter - data,
+			})
+	);
 
-  return { counter, incrementAsync, decrementAsync };
+	return { counter, incrementAsync, decrementAsync };
 };
